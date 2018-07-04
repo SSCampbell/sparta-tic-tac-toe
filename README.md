c8d8e7237dfe199278a990ff4bdf2707dd39d0d5
unknown <Scampbell@spartaglobal.com>
2018-07-05 00:22:04 +0100

    Function: I have worked on assigning values to the blocks and calling them when checking || README file updated

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# sparta-tic-tac-toe
+# Sparta Global Academy Homework -- Tic-Tac-Toe
+
+## Project Review:
+
+### In this project I had to implement a tic-tac-toe function to a basic game layout. Today the javaScript command that was revised was jQuery
+
+## Project Download:  
+### Download from the files created and transferred to GITHUB
+### The repository was created on GITHUB and cloned to access from terminal  
+### Used a JS file & HTML file
+
+## Using JavaScript tools such as:
+```JavaScript
+$("td").click(function(){
+if (player1Turn == true)
+{
+// checkWin();
+$(this).addClass("X").html("X");
+player1Turn = false;
+console.log(player1Turn)
+$("h2").html("It is O's turn");
+}})
+```
+
+## To make the program respond to the user's choice, a jQuery feature that focused on when you user clicks on the table. Depending on the turn base it would be "X" for the player OR "O" for the other player. I used a .html() tag with the selected class to change the text output to the screen. The next part of the project was to create a winning function which checks if there is a winner. I  managed to assign the values of each box with the number it is assigned with, this would be helpful when I a selected the boxes that I will be checking. I could not link my function effectively so I will review my code and develop a better strategy for the expected outcome.
--- script.js
+++ script.js
@@ -2,7 +2,22 @@ $(document).ready(function() {
 
   var player1Turn = true;
   var gameOver = false;
+  var x = 3;
 var $turn = $("h2");
+var $0 = $('[data-num~="0"]');
+var $1 = $('[data-num~="1"]')
+var $2 = $('[data-num~="2"]')
+var $3 = $('[data-num~="3"]')
+var $4 = $('[data-num~="4"]')
+var $5 = $('[data-test~="5"]')
+var $6 = $('[data-test~="6"]')
+var $7 = $('[data-test~="7"]')
+var $8 = $('[data-test~="8"]')
+
+
+
+// if (gameOver == false)
+if ($0.html() !== "X" && $1.html() !== "X" && $2.html() !== "X"){
 console.log(player1Turn)
 
   $("#reset").click(function(){
@@ -10,21 +25,44 @@ console.log(player1Turn)
     $("td").removeClass("X").html(" ")
     $("td").removeClass("O").html(" ")
   });
-
+////////////////////////////////////////////////
   $("td").click(function(){
   if (player1Turn == true)
   {
-  $(this).addClass("X").html("<span> X </span>");
+  // checkWin();
+  $(this).addClass("X").html("X");
   player1Turn = false;
   console.log(player1Turn)
   $("h2").html("It is O's turn");
   }
   else if (player1Turn == false)
   {
-  $(this).addClass("O").html("<span> O </span>");
+    // checkWin();
+  $(this).addClass("O").html("O");
   player1Turn = true;
   $("h2").html("It is X's turn");
   }
 
+  // function checkWin(){
+    //{
+      // gameOver = true;
+    //}
+  // }
 });
+
+}
+else {
+  console.log("TESTING")
+}
+console.log($0)
+console.log($1)
+console.log($2)
+
+// if (gameOver == true)
+// {
+//   console.log("TESTING")
+// }
+
+
+
 });
