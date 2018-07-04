4bc985951c02b690266f272c09981b6dc334e893
unknown <Scampbell@spartaglobal.com>
2018-07-04 17:15:15 +0100

    LAYOUT: When the game alternates whos turn it is it will showcase

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
   var player1Turn = true;
   var gameOver = false;
-
+var $turn = $("h2");
 console.log(player1Turn)
 
   $("#reset").click(function(){
@@ -17,11 +17,13 @@ console.log(player1Turn)
   $(this).addClass("X").html("<span> X </span>");
   player1Turn = false;
   console.log(player1Turn)
+  $("h2").html("It is O's turn");
   }
   else if (player1Turn == false)
   {
   $(this).addClass("O").html("<span> O </span>");
   player1Turn = true;
+  $("h2").html("It is X's turn");
   }
 
 });
