26e1c3a98dc4f2323f5937d32ac52c3debf78bce
unknown <Scampbell@spartaglobal.com>
2018-07-04 16:51:50 +0100

    START: Initialized connection and set up the first response to the user's choice on board

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  var player1Turn = true;
+  var gameOver = false;
+
+console.log(player1Turn)
+
+  $("td").click(function(){
+  if (player1Turn == true)
+  {
+  $(this).addClass("X").html("<span> X </span>");
+  player1Turn = false;
+  console.log(player1Turn)
+  }
+  else if (player1Turn == false)
+  {
+  $(this).addClass("O").html("<span> O </span>");
+  player1Turn = true;
+  }
+
+});
+});
